42d4566004a7faae8b1cdb8aa13d76f1422ceeff
Oláh Norbert <norbertolah292@gmail.com>
2026-02-12 13:12:08 +0100

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,10 +5,4 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  @Render('index')
-  getHello() {
-    return { message: this.appService.getHello() };
-  }
 }
